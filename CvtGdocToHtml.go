7161a50287a5cacb50c0165c23d939ab938ecda2
prr123 <priemen@gmail.com>
2022-04-08 12:23:50 +0200

    inital commit

--- CvtGdocToHtml.go
+++ CvtGdocToHtml.go
@@ -5,10 +5,8 @@
 // split into two packages
 // CvtGdocToTxt
 // CvtGdocToHtml
-// 
-// CvtGdocToTxtv1.go from rd_Gdocv4.go
-// v1 move test to Gdoc
-// 19/12 - add docId to input arguments
+//
+//
 
 package main
 
@@ -24,14 +22,27 @@ func main() {
 	var gd gdocApi.GdocApiStruct
 
     numArgs := len(os.Args)
-    if numArgs < 2 {
+    if numArgs < 3 {
 		cmd := os.Args[0]
-        fmt.Println("error - no comand line arguments!")
-          fmt.Printf("%s usage is:\n  %s docId\n", cmd[2:], cmd)
-        os.Exit(1)
+		switch numArgs {
+			case 2:
+        		fmt.Println("error - no comand line arguments!")
+          		fmt.Printf("%s usage is:\n  %s docId opt [sumary, main, all]\n", cmd[2:], cmd)
+        		os.Exit(1)
+			case 3:
+        		fmt.Println("error - no opt argument provided!")
+          		fmt.Printf("%s usage is:\n  %s docId opt [sumary, main, all]\n", cmd[2:], cmd)
+        		os.Exit(1)
+			case 4:
+			default:
+        		fmt.Println("error - too many arguments!")
+          		fmt.Printf("%s usage is:\n  %s docId opt [sumary, main, all]\n", cmd[2:], cmd)
+        		os.Exit(1)
+		}
     }
 
     docId := os.Args[1]
+	opt := os.Args[2]
 
 	err := gd.InitGdocApi()
 	srv := gd.Svc
@@ -43,20 +54,69 @@ func main() {
 	}
 	fmt.Printf("The title of the doc is: %s\n", doc.Title)
 
-	outFilNam := fmt.Sprintf("output/%s", doc.Title)
-	outfil, err := gd.CreOutFile(outFilNam, "html")
-	if err != nil {
-		fmt.Println("error main -- cannot open create outfile: ", err)
-		os.Exit(1)
-	}
-	err = gdocHtml.CreGdocHtmlFil(outfil, doc, nil)
-	if err != nil {
-		fmt.Println("error main -- cannot convert gdoc file: ", err)
-		os.Exit(1)
-	}
+	switch opt {
+	case "summary":
+		outFilNam := fmt.Sprintf("output/%s_summary", doc.Title)
+		outfil, err := gd.CreOutFile(outFilNam, "html")
+		if err != nil {
+			fmt.Println("error creOutFile -- cannot open create outfile: ", err)
+			os.Exit(1)
+		}
 
-	outfil.Close()
-	fmt.Println("success!")
-	os.Exit(0)
+		err = gdocHtml.CreGdocHtmlSummary(outfil, doc, nil)
+		if err != nil {
+			fmt.Println("error main: CreGdocHtmlSummary -- cannot convert gdoc doc: ", err)
+			os.Exit(1)
+		}
+		outfil.Close()
+		fmt.Println("*** success summary ***!")
+		os.Exit(0)
+
+	case "main":
+		outFilNam := fmt.Sprintf("output/%s_main", doc.Title)
+		outfil, err := gd.CreOutFile(outFilNam, "html")
+		if err != nil {
+			fmt.Println("error creOutFile-- cannot open create outfile: ", err)
+			os.Exit(1)
+		}
+
+		err = gdocHtml.CreGdocHtmlMain(outfil, doc, nil)
+		if err != nil {
+			fmt.Println("error main CreGdocHtmlMain -- cannot convert gdoc file: ", err)
+			os.Exit(1)
+		}
+		outfil.Close()
+		fmt.Println("*** success main ***!")
+		os.Exit(0)
+
+	case "all":
+		outFilNam := fmt.Sprintf("output/%s", doc.Title)
+		outfil, err := gd.CreOutFile(outFilNam, "html")
+		if err != nil {
+			fmt.Println("error creOutFil - cannot open create outfile: ", err)
+			os.Exit(1)
+		}
+
+		err = gdocHtml.CreGdocHtmlSummary(outfil, doc, nil)
+		if err != nil {
+			fmt.Println("error CreGdocHtmlSummary -- cannot convert gdoc file: ", err)
+			os.Exit(1)
+		}
+		err = gdocHtml.CreGdocHtmlMain(outfil, doc, nil)
+		if err != nil {
+			fmt.Println("error CreGdocHtmlMain -- cannot convert gdoc file: ", err)
+			os.Exit(1)
+		}
+
+		outfil.Close()
+		fmt.Println("*** success all ***!")
+		os.Exit(0)
+
+	default:
+		fmt.Printf("did not provide a valid opt: %s\n", opt)
+		fmt.Println("failure!")
+		os.Exit(1)
+	}
+	
 }
 
