2e777ffd4b4c9dbf53714bc12d4e8dd1542e1118
prr123 <priemen@gmail.com>
2022-04-26 13:10:13 +0200

    refactor output file logic

--- CvtGdocToTxt.go
+++ CvtGdocToTxt.go
@@ -14,6 +14,7 @@ package main
 import (
         "fmt"
         "os"
+		"strings"
 		gdocApi "google/gdoc/gdocApi"
 		gdocTxt "google/gdoc/gdocTxt"
 )
@@ -21,6 +22,9 @@ import (
 
 func main() {
 	var gd gdocApi.GdocApiStruct
+	// intialise
+    baseFolder := "output"
+    baseFolderSlash := baseFolder + "/"
 
     numArgs := len(os.Args)
 
@@ -31,33 +35,60 @@ func main() {
         os.Exit(2)
     }
 
-    docId := os.Args[1]
+ 	switch numArgs {
+        case 1:
+            fmt.Println("error - no comand line arguments!")
+            fmt.Printf("%s usage is:\n  %s docId\n", cmd[2:], cmd)
+            os.Exit(1)
+        case 2:
 
+		default:
+            fmt.Println("error - too many arguments!")
+            fmt.Printf("%s usage is:\n  %s docId\n", cmd[2:], cmd)
+            os.Exit(1)
+	}
+
+    docId := os.Args[1]
+	
 	err := gd.InitGdocApi()
 	srv := gd.Svc
 
+    outfilPath:= ""
+    switch {
+        case numArgs == 2:
+            outfilPath = baseFolder
+        case os.Args[2] == baseFolder:
+            outfilPath = os.Args[2]
+        case strings.Index(os.Args[2], baseFolderSlash)< 0:
+            outfilPath = baseFolderSlash + os.Args[2]
+        case strings.Index(os.Args[2], baseFolderSlash) == 0:
+            outfilPath = os.Args[2]
+        case os.Args[2] == "":
+            outfilPath = baseFolder
+        default:
+            fmt.Printf("no valid input folder: %s", os.Args[2])
+            os.Exit(1)
+    }
+
+
 //	docId := "1pdI_GFPR--q88V3WNKogcPfqa5VFOpzDZASo4alCKrE"
+
+
 	doc, err := srv.Documents.Get(docId).Do()
 	if err != nil {
 		fmt.Println("Unable to retrieve data from document: ", err)
 		os.Exit(1)
 	}
+
+    fmt.Printf("*************** CvtGdocToTxt ************\n")
 	fmt.Printf("The title of the doc is: %s\n", doc.Title)
 
-	outFilNam := fmt.Sprintf("output/%s", doc.Title)
-	outfil, err := gd.CreOutFile(outFilNam, "txt")
-	if err != nil {
-		fmt.Println("error main -- cannot open out file: ", err)
-		os.Exit(1)
-	}
-
-	err = gdocTxt.CvtGdocToTxt(outfil, doc)
+	err = gdocTxt.CvtGdocToTxt(outfilPath, doc)
 	if err != nil {
 		fmt.Println("error main -- cannot convert gdoc file: ", err)
 		os.Exit(1)
 	}
 
-	outfil.Close()
 	fmt.Println("Success!")
 	os.Exit(0)
 }
