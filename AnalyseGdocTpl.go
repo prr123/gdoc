b04a4ff27682118b74dad3ed80818a8c53147276
prr123 <priemen@gmail.com>
2022-12-18 11:31:47 +0100

    initial commit

---
+++ AnalyseGdocTpl.go
@@ -0,0 +1,24 @@
+// golang program that parses a gdoc document template
+// author: prr, azul software
+// created: 18/12/2022
+// copyright 2022 prr, Peter Riemenschneider, Azul Software
+//
+// start: AnalyseGdocTpl
+//
+
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func main() {
+
+
+
+
+
+
+
+}
